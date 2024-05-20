8a4db03b803b9b8b5d6095021709c6d0953c54ce
LUKASRIB15 <lucas.ribeiro06@aluno.ifce.edu.br>
2024-05-19 21:09:34 -0300

    adding the home screen and profile screen

---
+++ src/components/MovieNote/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const MovieNoteLayout = styled.button`
+  background-color: ${props=>props.theme.colors.pink};
+  padding: 3.2rem;
+  border-radius: 16px;
+  border: 0;
+  color: ${props=>props.theme.colors['gray-50']};
+  text-align: left;
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  cursor: pointer;
+
+  & > p{
+    margin-top: 1.6rem;
+    color: ${props=>props.theme.colors['gray-300']};
+    text-overflow: ellipsis;
+    overflow: hidden;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    display: -webkit-box;
+  }
+
+  & > footer{
+    margin-top: 1.6rem;
+    display: flex;
+    gap: 0.8rem;
+    flex-wrap: wrap;
+  }
+`
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+export const ProfileLayout = styled.div`
+  height: 100vh;
+
+  & > header{
+    background-color: ${props=>props.theme.colors.pink};
+    padding: 6.4rem 14.4rem;
+
+    div{
+      display: flex;
+      align-items: center;
+      gap: 0.8rem;
+
+      svg{
+        color: ${props=>props.theme.colors.red};
+      }
+    }
+  }
+
+
+`
+
+export const PickAvatar = styled.div`
+  background-color: ${props=>props.theme.colors.red};
+  width: max-content;
+  padding: 1.4rem 1.6rem;
+  cursor: pointer;
+  border-radius: 999px;
+  color: ${props=>props.theme.colors['gray-700']};
+
+  position: absolute;
+  bottom: 7px;
+  right: 7px;
+
+  .sr-only{
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border-width: 0;
+  }
+
+`
+
+export const ProfileForm = styled.form`
+  max-width: 340px;
+  margin: -84px auto 0;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.8rem;
+
+  img{
+    width: 186px;
+    height: 186px;
+    border-radius: 50%;
+  }
+
+
+  & > div:first-child > input{
+    display: none;
+  }
+
+  & > div:first-child{
+    margin-bottom: 6.4rem;
+    position: relative;
+  }
+
+  & > div:nth-child(4){
+    margin-top: 1.6rem;
+  }
+
+  & > button{
+    margin-top: 1.6rem;
+  }
+`
